c718944b7a52ede5bfdf70de1697f275213373b7
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-05-21 19:53:25 -0700

    new test2, test3 added

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 // Test1: AppendFirstNextTest
-TEST(MaxSubArraySum, AppendFirstNextTest) {
+TEST(LIST, AppendFirstNextTest) {
   LIST *list1 = ListCreate();
   LIST *list2 = ListCreate();
   
@@ -32,4 +32,46 @@ TEST(MaxSubArraySum, AppendFirstNextTest) {
   EXPECT_EQ(c, *(int*)ListNext(list2));
   EXPECT_EQ(d, *(int*)ListNext(list2));
   EXPECT_EQ(e, *(int*)ListNext(list2));
+}
+
+// Test2: addAtDesignedPlace
+TEST(MaxSubArraySum, addAtDesignedPlace) {
+  LIST *list = ListCreate();
+  
+  int a = 5;
+  ListAppend(list, &a);
+  
+  int b = 6;
+  ListAppend(list, &b);
+
+  int c = 7;
+  ListAppend(list, &c);
+
+  int d = 8;
+  ListAppend(list, &d);
+  
+  ListFirst(list); // current pointer at head 5
+  ListNext(list);  // current pointer at 6
+  ListNext(list);  // current pointer at 7
+//  ListNext(temp);  // current pointer at tail 8
+//  ListNext(temp);  // current pointer beyond tail
+  
+  int e = 9;
+  EXPECT_EQ(0, ListAdd(list, &e));
+
+  EXPECT_EQ(a, *(int*)ListFirst(list));
+  EXPECT_EQ(b, *(int*)ListNext(list));
+  EXPECT_EQ(c, *(int*)ListNext(list));
+  EXPECT_EQ(e, *(int*)ListNext(list));
+  EXPECT_EQ(d, *(int*)ListNext(list));
+}
+
+// Test3: createAndAdd
+TEST(MaxSubArraySum, createAndAdd) {
+  LIST *list = ListCreate();
+  
+  int a = 5;
+  ListAdd(list, &a);
+  
+  EXPECT_EQ(a, *(int*)ListFirst(list));
 }
--- testTemp/test5.c
+++ testTemp/test5.c
@@ -10,18 +10,8 @@ int main(void)
   int a = 5;
   ListAdd(temp, &a);
   
-//  ListFirst(temp); // current pointer at head 5
-//  ListNext(temp);  // current pointer at 6
-//  ListNext(temp);  // current pointer at 7
-//  ListNext(temp);  // current pointer at tail 8
-//  ListNext(temp);  // current pointer beyond tail
-  
   
   printf("%d\n", *(int*)ListFirst(temp));
-//  printf("%d ", *(int*)ListNext(temp));
-//  printf("%d ", *(int*)ListNext(temp));
-//  printf("%d ", *(int*)ListNext(temp));
-//  printf("%d\n", *(int*)ListNext(temp));
   
   return 0;
 }
