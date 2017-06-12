88726b37efc89bbbb8d29e3aec53736c895603da
ChaunceyKiwi <chaunceykiwi@gmail.com>
2017-06-12 15:25:36 -0700

    a bug fixed
    a bug fixed on makefile, now /include is added to the search path

--- Assignment1/src/test.c
+++ Assignment1/src/test.c
@@ -21,7 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "../include/list.h"
+#include "list.h"
 
 /***********************************************************
 *   User defined functions declaration
